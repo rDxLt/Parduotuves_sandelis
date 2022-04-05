c0e7e25bf4e2f09195274c3b342f739bcea38c0d
Robertas <robertas.badikonis@gmail.com>
2022-04-05 21:35:51 +0300

    created login at home page

--- index.php
+++ index.php
@@ -1,1 +1,17 @@
 <?php
+
+
+?>
+
+<form action="/action_page.php">
+    <fieldset>
+        <legend>Prisijungimas:</legend>
+        Paštas : <input type="email" id="email" name="email">
+        <br><br>
+        Slaptažodis: <input type="password" id="password" name="password">
+        <br><br>
+        <input type="submit" value="Prisijungti">
+        <hr>
+        <a href="#">Registracija</a>
+    </fieldset>
+</form>
