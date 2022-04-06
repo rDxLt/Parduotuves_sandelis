a34fae02b632684aaadbb29d60a6559e0613b0da
Robertas <robertas.badikonis@gmail.com>
2022-04-06 12:36:42 +0300

    modyfied login table to look better

--- index.php
+++ index.php
@@ -3,13 +3,29 @@
 
 ?>
 
-<form action="/action_page.php">
+<form action="/action_page.php" method="post">
     <fieldset>
         <legend>Prisijungimas:</legend>
-        Paštas : <input type="email" id="email" name="email">
-        <br><br>
-        Slaptažodis: <input type="password" id="password" name="password">
-        <br><br>
+        <table>
+            <tr>
+                <td>
+                    Paštas:
+                </td>
+                <td>
+                    <input type="email" id="email" name="email">
+                </td>
+            </tr>
+            <br>
+            <tr>
+                <td>
+                    Slaptažodis:
+                </td>
+                <td>
+                    <input type="password" id="password" name="password">
+                </td>
+            </tr>
+        </table>
+        <br>
         <input type="submit" value="Prisijungti">
         <hr>
         <a href="#">Registracija</a>
