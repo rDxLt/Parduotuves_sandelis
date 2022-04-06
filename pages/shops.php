4512110eba249a8bcfb44ceb1662f7910dfe9a08
Robertas <robertas.badikonis@gmail.com>
2022-04-06 15:26:43 +0300

    added all shops in a dropdown to choose

--- pages/shops.php
+++ pages/shops.php
@@ -1,1 +1,18 @@
 <h1>Parduotuvės</h1>
+<table>
+    <tr>
+        <td>
+            Pasirinkite parduotuvę:
+        </td>
+        <td>
+            <select type="number" name="category_id" value=""><br/>
+                <?php
+                $result = mysqli_query($database, 'select * from parduotuves');
+                $parduotuves = mysqli_fetch_all($result, MYSQLI_ASSOC);
+                foreach ($parduotuves as $parduotuve) { ?>
+                    <option value="<?php echo $parduotuve['pavadinimas'] ?>"><?php echo $parduotuve['pavadinimas'] ?></option>
+                <?php } ?>
+            </select><br/>
+        </td>
+    </tr>
+</table>
