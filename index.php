33b662df6ab515c2c7f8615ebd3f2bc6debc4815
Robertas <robertas.badikonis@gmail.com>
2022-04-06 12:57:33 +0300

    removed unuseful links

--- index.php
+++ index.php
@@ -1,33 +1,62 @@
 <?php
+include_once 'config.php';
+?>
+<hr>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Hello, world!</title>
+</head>
+<body>
+<style>
+    table {
+        padding: 10px;
+    }
 
+    td {
+        padding: 10px;
+    }
+</style>
+<!--menu-->
+<table>
+    <tr>
+        <td>
+            <a href="index.php">Home</a>
+        </td>
+        <?php if (isLoged() === false) { ?>
+            <td>
+                <a href="index.php?page=login">Login</a>
+            </td>
+            <td>
+                <a href="index.php?page=register">Register</a>
+            </td>
+        <?php } ?>
+        <?php if (isLoged() === true) { ?>
+            <td>
+                <a href="index.php?page=logout">Atsijungti</a>
+            </td>
+        <?php } ?>
+    </tr>
+</table>
 
+<?php
+if ($page === null) {
+    include 'pages/home.php';
+} elseif ($page === 'register') {
+    include 'pages/registration.php';
+} elseif ($page === 'login') {
+    include 'pages/login.php';
+} elseif ($page === 'logout') {
+    include 'pages/logout.php';
+}
 ?>
 
-<form action="/action_page.php" method="post">
-    <fieldset>
-        <legend>Prisijungimas:</legend>
-        <table>
-            <tr>
-                <td>
-                    Paštas:
-                </td>
-                <td>
-                    <input type="email" id="email" name="email">
-                </td>
-            </tr>
-            <br>
-            <tr>
-                <td>
-                    Slaptažodis:
-                </td>
-                <td>
-                    <input type="password" id="password" name="password">
-                </td>
-            </tr>
-        </table>
-        <br>
-        <input type="submit" value="Prisijungti">
-        <hr>
-        <a href="#">Registracija</a>
-    </fieldset>
-</form>
+<br/><br/>
+<!--footer-->
+<?php
+echo date('Y-m-d H:i:s');
+?>
+</body>
+</html>
