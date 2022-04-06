ca11fced8206c7ac6fff879ea30ed2695afad071
Robertas <robertas.badikonis@gmail.com>
2022-04-06 20:34:00 +0300

    created warehouse product add form

--- pages/warehouse.php
+++ pages/warehouse.php
@@ -1,1 +1,43 @@
 <h1>Sandėlys</h1>
+
+<h3>Produkto pridejimo forma</h3>
+<form action="#" method="post">
+    <table>
+        <tr>
+            <td>
+                Kategorija:
+            </td>
+            <td>
+                <input type="text" name="product_category" value="">
+                <br/>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                Pavadinimas:
+            </td>
+            <td>
+                <input type="number" name="product_name" value="">
+                <br/>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                Kaina:
+            </td>
+            <td>
+                <input type="number" name="product_price" value=""><br/>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                Galiojimo dienos:
+            </td>
+            <td>
+                <input type="number" name="product_use_by_date" value=""><br/>
+            </td>
+        </tr>
+        <br/>
+    </table>
+    <br/>
+    <button type="submit">Pridėti</button>
