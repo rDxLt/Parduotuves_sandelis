cda8a2d991d6d7bbf67576f2c8d49a06a53eed57
Robertas <robertas.badikonis@gmail.com>
2022-04-07 12:21:33 +0300

    added products add form and list in products.php

--- pages/products.php
+++ pages/products.php
@@ -1,1 +1,98 @@
+<?php
+
+if (isset($_POST['name'])) {
+    $id = $_POST['id'];
+    $category = $_POST['category'];
+    $name = $_POST['name'];
+    $price = $_POST['price'];
+    $useBy_date = $_POST['useBy_date'];
+
+    $sql = mysqli_query($database, 'insert into produktai (id, kategorija, pavadinimas, kaina, galiojimo_dienos) value ("' . $id . '","' . $category . '","' . $name . '","' . $price . '","' . $useBy_date . '")');
+}
+
+
+?>
 <h1>Produktai</h1>
+<h2>
+    Produktų sąrašas:
+</h2>
+<table>
+    <tr>
+        <th>ID</th>
+        <th>Kategorija</th>
+        <th>Pavadinimas</th>
+        <th>Kaina €</th>
+        <th>Galiojimo dienos</th>
+    </tr>
+    <?php
+    $result = mysqli_query($database, 'select * from produktai');
+    $products = mysqli_fetch_all($result, MYSQLI_ASSOC);
+    foreach ($products as $product) { ?>
+        <tr>
+            <td>
+                <?php echo $product['id'] ?>
+            </td>
+            <td>
+                <?php echo $product['kategorija'] ?>
+            </td>
+            <td>
+                <?php echo $product['pavadinimas'] ?>
+            </td>
+            <td>
+                <?php echo $product['kaina'] ?>
+            </td>
+            <td>
+                <?php echo $product['galiojimo_dienos'] ?>
+            </td>
+        </tr>
+    <?php } ?>
+</table>
+
+<h3>Produkto pridėjimo forma</h3>
+<form action="#" method="post">
+    <table>
+        <tr>
+            <td>
+                Prekės ID:
+            </td>
+            <td>
+                <input type="number" name="id" value="">
+            </td>
+        </tr>
+        <tr>
+            <td>
+                Kategorija:
+            </td>
+            <td>
+                <input type="text" name="category" value="">
+            </td>
+        </tr>
+        <tr>
+            <td>
+                Pavadinimas:
+            </td>
+            <td>
+                <input type="text" name="name" value="">
+            </td>
+        </tr>
+        <tr>
+            <td>
+                Kaina:
+            </td>
+            <td>
+                <input type="text" name="price" value=""><br/>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                Galiojimo dienos:
+            </td>
+            <td>
+                <input type="number" name="useBy_date" value=""><br/>
+            </td>
+        </tr>
+        <!--        <br/>-->
+    </table>
+    <!--    <br/>-->
+    <button type="submit">Pridėti</button>
+
