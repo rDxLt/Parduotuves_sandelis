30f46f556c05908e70836d388333800e04708973
Robertas <robertas.badikonis@gmail.com>
2022-04-08 21:57:53 +0300

    added delete href to product table in warehouse.php

--- pages/warehouse.php
+++ pages/warehouse.php
@@ -1,95 +1,88 @@
 <?php
 
-if (isset($_POST['name'])) {
+if (isset($_POST['id'])) {
     $id = $_POST['id'];
-    $category = $_POST['category'];
-    $name = $_POST['name'];
-    $price = $_POST['price'];
-    $useBy_date = $_POST['useBy_date'];
+    $amount = $_POST['likutis'];
 
-    $sql = mysqli_query($database, 'insert into produktai (id, kategorija, pavadinimas, kaina, galiojimo_dienos) value ("' . $id . '","' . $category . '","' . $name . '","' . $price . '","' . $useBy_date . '")');
+    $sql = 'insert into sandelio_produktai (produkto_id, likutis) value ("' . $id . '","' . $amount . '")';
+    $result = mysqli_query($database, $sql);
+//    echo '<pre>';
+//    print_r($sql);
+//    echo '</pre>';
+
 }
-
 
 ?>
 
-<h2>
-    Prekių sąrašas sandėlyje:
-</h2>
-<table>
-    <tr>
-        <th>ID</th>
-        <th>Pavadinimas</th>
-        <th>Kaina €</th>
-        <th>Galiojimo dienos</th>
-    </tr>
-    <?php
-    $result = mysqli_query($database, 'select * from produktai');
-    $products = mysqli_fetch_all($result, MYSQLI_ASSOC);
-    foreach ($products as $product) { ?>
-        <tr>
-            <td>
-                <?php echo $product['id'] ?>
-            </td>
-            <td>
-                <?php echo $product['pavadinimas'] ?>
-            </td>
-            <td>
-                <?php echo $product['kaina'] ?>
-            </td>
-            <td>
-                <?php echo $product['galiojimo_dienos'] ?>
-            </td>
-        </tr>
-    <?php } ?>
-</table>
-
-<h1>Sandėlys</h1>
-<h3>Produkto pridėjimo forma</h3>
+<h3>Produkto užsakymo forma</h3>
 <form action="#" method="post">
-    <table>
+    <table border="1px">
         <tr>
             <td>
-                Prekės ID:
+                Prekė:
             </td>
             <td>
-                <input type="number" name="id" value="">
+                <select type="number" name="id" value=""><br/>
+                    <?php
+                    $result = mysqli_query($database, 'select * from produktai');
+                    $products = mysqli_fetch_all($result, MYSQLI_ASSOC);
+                    foreach ($products as $product) { ?>
+                        <option value="<?php echo $product['id'] ?>"><?php echo $product['pavadinimas'] ?></option>
+                    <?php } ?>
+                </select><br/>
             </td>
         </tr>
         <tr>
             <td>
-                Kategorija:
+                Kiekis:
             </td>
             <td>
-                <input type="text" name="category" value="">
+                <input type="number" name="likutis" value="">
             </td>
         </tr>
-        <tr>
-            <td>
-                Pavadinimas:
-            </td>
-            <td>
-                <input type="text" name="name" value="">
-            </td>
-        </tr>
-        <tr>
-            <td>
-                Kaina:
-            </td>
-            <td>
-                <input type="text" name="price" value=""><br/>
-            </td>
-        </tr>
-        <tr>
-            <td>
-                Galiojimo dienos:
-            </td>
-            <td>
-                <input type="number" name="useBy_date" value=""><br/>
-            </td>
-        </tr>
-        <br/>
     </table>
     <br/>
     <button type="submit">Pridėti</button>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <h2>
+        Produktų sąrašas sandėlyje:
+    </h2>
+    <table border=1px>
+        <tr>
+            <!--            <th>ID</th>-->
+            <th>Pavadinimas</th>
+            <th>Likutis</th>
+            <th>Action</th>
+        </tr>
+        <?php
+        $result = mysqli_query($database, 'select * from produktai');
+        $products = mysqli_fetch_all($result, MYSQLI_ASSOC);
+        foreach ($products
+
+        as $product) { ?>
+        <tr>
+            <!--            <td>-->
+            <!--                --><?php //echo $product['id']; ?>
+            <!--            </td>-->
+            <td align="center">
+                <?php echo $product['pavadinimas'] ?>
+            </td>
+            <td>
+                <!--                --><?php
+                //                $sandelioPrekes = mysqli_query($database, 'select * from sandelio_produktai');
+                //                $sandelioPrekes = mysqli_fetch_all($sandelioPrekes, MYSQLI_ASSOC);
+                //                foreach ($sandelioPrekes as $sandelioPreke) {
+                //                    echo $sandelioPreke['likutis'] ?>
+                <!--                --><?php //} ?>
+            </td>
+            <td>
+                <a href="delete.php?=id">Delete</a>
+            </td>
+
+            <?php } ?>
+        </tr>
+    </table>
 
